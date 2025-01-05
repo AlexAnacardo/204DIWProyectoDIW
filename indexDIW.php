d8f91548772d4c46985de491786f89d8ea3197e1
Alex <Alex@PC-ALEX.home>
2025-01-05 15:06:27 +0100

    1er commit -Creado indice de la asignatura y enlazados ejercicios

---
+++ indexDIW.php
@@ -0,0 +1,178 @@
+<html>
+     <head>
+        <meta charset="UTF-8">               
+        <meta name="author" content="Alex Asensio Sanchez">
+        <meta name="application-name" content="indice">        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+         <link rel="stylesheet" href="webroot/css/indexDIW.css">
+         <title>Alex Asensio Sanchez</title>
+     </head>
+     <body>
+         <header>
+             <h1>DISEÑO DE APLICACIONES WEB</h1>
+         </header>       
+        <main>                
+            <table>
+                <thead>
+                    <th>nº</th>
+                    <th>Enunciado</th>
+                    <th>Ejecutar</th>                    
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1-1-1</td>
+                        <td>Color</td>
+                        <td><a href="PR-1-1-1-Color.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>                                                
+                    </tr>
+                    <tr>
+                        <td>1-1-2</td>
+                        <td>Colores complementarios</td>
+                        <td><a href="PR-1-1-2 Colores complementarios.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
+                    
+                    </tr>
+                    <tr>
+                        <td>1-2-1</td>
+                        <td>Tipografia 1</td>
+                        <td><a href="PR-1-2-1 Tipografia1.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
+                    </tr>
+                    <tr>
+                        <td>1-2-1</td>
+                        <td></td>
+                        <td><a href="PR-1-2-1.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
+                    </tr>
+                    <tr>
+                        <td>1-2-3</td>
+                        <td></td>
+                        <td><a href="PR-1-2-3.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>                        
+                    </tr>
+                    <tr>
+                        <td>2-3</td>
+                        <td></td>
+                        <td><a href="PR-2-3.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>                        
+                    </tr>
+                    <tr>
+                        <td>2-4</td>
+                        <td></td>
+                        <td><a href="PR-2-4"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>                        
+                    </tr>
+                    <tr>
+                        <td>2-4-2</td>
+                        <td></td>
+                        <td><a href="PR-2-4-2.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>                        
+                    </tr>
+                    <tr>
+                        <td>2-4-3</td>
+                        <td></td>
+                        <td><a href="PR-2-4-3.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>                        
+                    </tr>
+                    <tr>
+                        <td>2-5</td>
+                        <td></td>
+                        <td><a href="PR-2-5.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>                        
+                    </tr>
+                    <tr>
+                        <td>2-6</td>
+                        <td></td>
+                        <td><a href="PR-2-6.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
+                    </tr>
+                    <tr>
+                        <td>2-7</td>
+                        <td></td>
+                        <td><a href="PR-2-7.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
+                    </tr>
+                    <tr>
+                        <td>2-8-1</td>
+                        <td></td>
+                        <td><a href="PR-2-8-1.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>      
+                    </tr>
+                    <tr>
+                        <td>2-8-1-b</td>
+                        <td></td>
+                        <td><a href="PR-2-8-1-b.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>           
+                    </tr>
+                    <tr>
+                        <td>2-8-2 a</td>
+                        <td></td>
+                        <td><a href="PR-2-8-2 a.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>       
+                    </tr>
+                    <tr>
+                        <td>2-8-2 b</td>
+                        <td></td>
+                        <td><a href="PR-2-8-2 b.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>  
+                    </tr>
+                    <tr>
+                        <td>2-8-3</td>
+                        <td></td>
+                        <td><a href="PR-2-8-3.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>      
+                    </tr>
+                    <tr>
+                        <td>2-8-5</td>
+                        <td></td>
+                        <td><a href="PR-2-8-5.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>  
+                    </tr>
+                    <tr>
+                        <td>2-8-5-2</td>
+                        <td></td>
+                        <td><a href="PR-2-8-5-2.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
+                    </tr>
+                    <tr>
+                        <td>2-8-5-3</td>
+                        <td></td>
+                        <td><a href="PR-2-8-5-3.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
+                    </tr>
+                    <tr>
+                        <td>2-8-6</td>
+                        <td></td>
+                        <td><a href="PR-2-8-6.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
+                    </tr>
+                    <tr>
+                        <td>2-9-1</td>
+                        <td></td>
+                        <td><a href="PR-2-9-1.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
+                    </tr>
+                    <tr>
+                        <td>2-9-2</td>
+                        <td></td>
+                        <td><a href="PR-2-9-2.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>                        
+                    </tr>
+                    <tr>
+                        <td>2-9-3-b</td>
+                        <td></td>
+                        <td><a href="PR-2-9-3-b.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
+                        
+                        
+                    </tr>
+                    <tr>
+                        <td>PR-2-9-3-c</td>
+                        <td></td>
+                        <td><a href="PR-2-9-3-c.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
+                    </tr>
+                    <tr>
+                        <td>2-9-4</td>
+                        <td></td>
+                        <td><a href="PR-2-9-4.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>     
+                    </tr>
+                    <tr>
+                        <td>2-9-5</td>
+                        <td></td>
+                        <td><a href="PR-2-9-5.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>         
+                    </tr>
+                    <tr>
+                        <td>2-9-6</td>
+                        <td></td>
+                        <td><a href="PR-2-9-6.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
+                    </tr>
+                    <tr>
+                        <td>PR-2-9-7</td>
+                        <td></td>
+                        <td><a href="PR-2-9-7.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
+                    </tr>                                     
+                </tbody>
+            </table>           
+        </main>
+        <footer>
+            <p><a href="../index.html">Alex Asensio Sanchez</a></p>            
+            <p><a target="blank" href="https://github.com/AlexAnacardo/204DWESProyectoTema4">Repositorio del proyecto</a></p>
+        </footer>
+     </body>
+ </html>
