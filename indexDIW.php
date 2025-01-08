0519a85c3ce4b08f4fa3fcc2796d30240087a33b
Alex <alex.asesan@educa.jcyl.es>
2025-01-08 13:05:56 +0100

    Finalizada practica 2-9-7

--- indexDIW.php
+++ indexDIW.php
@@ -1,16 +1,16 @@
 <html>
-     <head>
+    <head>
         <meta charset="UTF-8">               
         <meta name="author" content="Alex Asensio Sanchez">
         <meta name="application-name" content="indice">        
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-         <link rel="stylesheet" href="webroot/css/indexDIW.css">
-         <title>Alex Asensio Sanchez</title>
-     </head>
-     <body>
-         <header>
-             <h1>DISEÑO DE APLICACIONES WEB</h1>
-         </header>       
+        <link rel="stylesheet" href="webroot/css/indexDIW.css">
+        <title>Alex Asensio Sanchez</title>
+    </head>
+    <body>
+        <header>
+            <h1>DISEÑO DE APLICACIONES WEB</h1>
+        </header>       
         <main>                
             <table>
                 <thead>
@@ -37,82 +37,82 @@
                     </tr>
                     <tr>
                         <td>1-2-1</td>
-                        <td></td>
+                        <td>Colocacion de contenedores</td>
                         <td><a href="PR-1-2-1.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
                     </tr>
                     <tr>
                         <td>1-2-3</td>
-                        <td></td>
+                        <td>Tablas</td>
                         <td><a href="PR-1-2-3.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>                        
                     </tr>
                     <tr>
                         <td>2-3</td>
-                        <td></td>
+                        <td>Animaciones con hover</td>
                         <td><a href="PR-2-3.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>                        
                     </tr>
                     <tr>
                         <td>2-4</td>
-                        <td></td>
+                        <td>Selectores CSS</td>
                         <td><a href="PR-2-4.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>                        
                     </tr>
                     <tr>
                         <td>2-4-2</td>
-                        <td></td>
+                        <td>Selectores CSS 2</td>
                         <td><a href="PR-2-4-2.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>                        
                     </tr>
                     <tr>
                         <td>2-4-3</td>
-                        <td></td>
+                        <td>Uso de target</td>
                         <td><a href="PR-2-4-3.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>                        
                     </tr>
                     <tr>
                         <td>2-5</td>
-                        <td></td>
+                        <td>Formulario</td>
                         <td><a href="PR-2-5.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>                        
                     </tr>
                     <tr>
                         <td>2-6</td>
-                        <td></td>
+                        <td>Uso de Z-Index</td>
                         <td><a href="PR-2-6.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
                     </tr>
                     <tr>
                         <td>2-7</td>
-                        <td></td>
+                        <td>Colocacion con selectores CSS</td>
                         <td><a href="PR-2-7.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
                     </tr>
                     <tr>
                         <td>2-8-1</td>
-                        <td></td>
+                        <td>Uso de Z-Index y target</td>
                         <td><a href="PR-2-8-1.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>      
                     </tr>
                     <tr>
                         <td>2-8-1-b</td>
-                        <td></td>
+                        <td>Uso de Z-Index y target 2</td>
                         <td><a href="PR-2-8-1-b.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>           
                     </tr>
                     <tr>
                         <td>2-8-2 a</td>
-                        <td></td>
+                        <td>Menu desplegable usando display</td>
                         <td><a href="PR-2-8-2 a.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>       
                     </tr>
                     <tr>
                         <td>2-8-2 b</td>
-                        <td></td>
+                        <td>Ventana modal con :checked y radioButtons</td>
                         <td><a href="PR-2-8-2 b.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>  
                     </tr>
                     <tr>
                         <td>2-8-3</td>
-                        <td></td>
+                        <td>Menu lateral con animacion</td>
                         <td><a href="PR-2-8-3.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>      
                     </tr>
                     <tr>
                         <td>2-8-5</td>
-                        <td></td>
+                        <td>Animacion en boton al hacer hover</td>
                         <td><a href="PR-2-8-5.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>  
                     </tr>
                     <tr>
                         <td>2-8-5-2</td>
-                        <td></td>
+                        <td>Bola con gradientes y sombra usando opacity</td>
                         <td><a href="PR-2-8-5-2.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
                     </tr>
                     <tr>
@@ -122,47 +122,47 @@
                     </tr>
                     <tr>
                         <td>2-8-6</td>
-                        <td></td>
+                        <td>Cajas con picos</td>
                         <td><a href="PR-2-8-6.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
                     </tr>
                     <tr>
                         <td>2-9-1</td>
-                        <td></td>
+                        <td>Colocacion de elementos</td>
                         <td><a href="PR-2-9-1.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
                     </tr>
                     <tr>
                         <td>2-9-2</td>
-                        <td></td>
+                        <td>Pagina responsive usando @media</td>
                         <td><a href="PR-2-9-2.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>                        
                     </tr>
                     <tr>
                         <td>2-9-3-b</td>
-                        <td></td>
+                        <td>Posicionamiento de los puntos de un dado</td>
                         <td><a href="PR-2-9-3-b.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
                     </tr>
                     <tr>
-                        <td>PR-2-9-3-c</td>
-                        <td></td>
+                        <td>2-9-3-c</td>
+                        <td>Animacion ensanchar y girar verticalmente un elemento al hacer hover</td>
                         <td><a href="PR-2-9-3-c.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
                     </tr>
                     <tr>
                         <td>2-9-4</td>
-                        <td></td>
+                        <td>Pagina de compra de relojes</td>
                         <td><a href="PR-2-9-4.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>     
                     </tr>
                     <tr>
                         <td>2-9-5</td>
-                        <td></td>
+                        <td>Menu responsive</td>
                         <td><a href="PR-2-9-5.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>         
                     </tr>
                     <tr>
                         <td>2-9-6</td>
-                        <td></td>
+                        <td>Tablas con div</td>
                         <td><a href="PR-2-9-6.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
                     </tr>
                     <tr>
-                        <td>PR-2-9-7</td>
-                        <td></td>
+                        <td>2-9-7</td>
+                        <td>Carrusel de fotos</td>
                         <td><a href="PR-2-9-7.html"><image src="webroot/images/play.png" heigth="25%" width="25%"/></a></td>
                     </tr>                                     
                 </tbody>
